6a9bb81f9ddad282d3fd11ab1ee6833a804942f9
Oona Pecson <op531@nyu.edu>
2020-10-26 14:42:33 -0400

    Namsan Made

---
+++ namsan-tower/sketch.js
@@ -0,0 +1,33 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight + 150);
+}
+
+function draw() {
+  background(255);
+
+  fill (86, 133, 186);
+  noStroke();
+  rect(100, 100, 500, 500);
+
+  let scale = 1 / 10
+  let scale2 = 2.5 / 10
+
+  imageMode(CENTER);
+  image(img, 350, 270, img.width * scale, img.height * scale);
+  image(img2, 900, 470, img.width * scale2, img.height * scale2);
+  position = ('SCALE');
+
+}
+
+function preload() {
+  img = loadImage('assets/seoul/NAMSAN.jpg');
+  img2 = loadImage('assets/notepad.PNG');
+}
+
+function openWin() {
+  window.open("http://www.nseoultower.co.kr/eng/");
+}
+function openWin2() {
+  window.open("../index.html");
+}
