98da952de4b35f82da41eb50a40abe2126ac022f
Oona Pecson <op531@nyu.edu>
2020-10-31 22:37:36 -0400

    re-added seoul page

---
+++ seoul.js
@@ -0,0 +1,29 @@
+let img;
+let img2;
+
+function preload() {
+  img = loadImage('assets/seoul/NAMSAN.PNG');
+  img2 = loadImage('assets/seoul/HONGDAE.PNG')
+}
+
+function setup() {
+  createCanvas(1280, 770)
+}
+
+function draw() {
+  let scale = 1.2 / 10
+  let scale2 = 2 / 10
+  imageMode(CENTER);
+  image(img, 1050, 220, img.width * scale, img.height * scale);
+  image(img2, 350, 480, img.width * scale2, img.height * scale2);
+  position = ('SCALE');
+
+}
+
+function openWin() {
+  window.open("namsan-tower/index2.html");
+}
+
+function openWin2() {
+  window.open("hongdae/index3.html");
+}
