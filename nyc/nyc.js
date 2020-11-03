e30bf63f3f832a19f79a3a6435fa8f0781af54c4
Oona Pecson <op531@nyu.edu>
2020-11-03 11:53:28 -0500

    Trying to fix alignment

--- nyc/nyc.js
+++ nyc/nyc.js
@@ -16,19 +16,20 @@ function draw() {
   imageMode(CENTER);
   image(img, 905, 380, img.width * scale, img.height * scale); 
   image(img2, 650, 420, img.width * scale2, img.height * scale2);
-  position = ('FIXED');
+  position = ('SCALE');
 
 }
 
 function openWin() {
-  window.open("empire/empire-index.html");
+  window.open("empire/empire-index.html")
+
 }
 
 function openWin2() {
-  window.open("chinatown/ctown-index.html"); 
+  window.open("chinatown/ctown-index.html")
 }
 
 function openWin3() {
-  window.open("../index.html");
+  window.open("../index.html")
 }
 
